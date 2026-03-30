1cdfb200ee31469792df11fa0efeb4c8099cd6a7
elvi4 <maksim.gritsuk@bk.ru>
2026-03-30 16:51:13 +0300

    Initial Commit

---
+++ ChianParser/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ChianParser
+//
+//  Created by Maksim on 30.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ChianParserTests/ChianParserTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChianParserTests.swift
+//  ChianParserTests
+//
+//  Created by Maksim on 30.03.2026.
+//
+
+import Testing
+@testable import ChianParser
+
+struct ChianParserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
